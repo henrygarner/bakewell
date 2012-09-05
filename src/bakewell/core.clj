e2f28cd1634f0df046bdf9723dffa556a54b29ee
Henry Garner <henry@henrygarner.com>
2012-09-05 20:22:38 +0100

    Minor refactor

--- src/bakewell/core.clj
+++ src/bakewell/core.clj
@@ -1,9 +1,10 @@
 (ns bakewell.core
   (:use  [biscuit.core :as crc]))
 
+(def start-byte 0xd5)
+
 (defn wrap-payload
   "Turn the payload into a message"
   [payload]
-  (reduce into [[0xd5 (count payload)]
-                payload
-                [(crc/checksum payload)]]))
+  (conj (into [start-byte (count payload)] payload)
+        (crc/checksum payload)))
